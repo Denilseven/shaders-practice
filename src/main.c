8523b6a5f6105b5c6f41f11ea0c73ff54708832a
Denilseven <104108067+Denilseven@users.noreply.github.com>
2026-04-21 08:30:36 -0400

    remove `target` (it isn't even needed somehow)
    bruh what is going on

--- src/main.c
+++ src/main.c
@@ -2,21 +2,21 @@
 #include <stdlib.h>
 #include <stdio.h>
 
-#define SCREEN_WIDTH 800
-#define SCREEN_HEIGHT 800
+#define SCREEN_WIDTH 1000
+#define SCREEN_HEIGHT 500
 
 int main() {
     InitWindow(SCREEN_WIDTH, SCREEN_HEIGHT, "cool");
+    SetWindowState(FLAG_WINDOW_RESIZABLE);
     SetTargetFPS(60);
 
     Shader loadedShader = LoadShader(0, "shaders/firstcloud.frag");
 
-    // following the example at https://github.com/raysan5/raylib/blob/master/examples/shaders/shaders_postprocessing.c
-    RenderTexture2D target = LoadRenderTexture(SCREEN_WIDTH, SCREEN_HEIGHT);
-
     while (!WindowShouldClose()) {
+        Vector2 dynamicScreenSize = (Vector2){GetScreenWidth(), GetScreenHeight()};
+
         float time = GetTime();
-        float res[2] = {SCREEN_WIDTH, SCREEN_HEIGHT}; 
+        float res[2] = {dynamicScreenSize.x, dynamicScreenSize.y}; 
         float mouse[2] = {GetMouseX(), GetMouseY()};
 
         int timeLoc = GetShaderLocation(loadedShader, "u_time");
@@ -26,18 +26,16 @@ int main() {
         SetShaderValue(loadedShader, timeLoc, &time, SHADER_UNIFORM_FLOAT);
         SetShaderValue(loadedShader, resLoc, res, SHADER_UNIFORM_VEC2);
         SetShaderValue(loadedShader, mouseLoc, mouse, SHADER_UNIFORM_VEC2);
-        // printf("time: %d res: %d mouse: %d\n", timeLoc, resLoc, mouseLoc);
 
         BeginDrawing();
-            ClearBackground(WHITE);
-            DrawCircle(0, 0, 10, BLUE);
+        ClearBackground(WHITE);
+        DrawCircle(0, 0, 10, BLUE);
 
-            BeginShaderMode(loadedShader);
-                DrawRectangle(0, 0, SCREEN_WIDTH, SCREEN_HEIGHT, WHITE);
-                DrawTextureRec(target.texture, (Rectangle){0, 0, (float)target.texture.width, (float)-target.texture.height}, (Vector2){0, 0}, WHITE);
-            EndShaderMode();
-        
-            DrawFPS(10, 10);
+        BeginShaderMode(loadedShader);
+        DrawRectangle(0, 0, dynamicScreenSize.x, dynamicScreenSize.y, WHITE);
+        EndShaderMode();
+    
+        DrawFPS(10, 10);
         EndDrawing();
     }
 
